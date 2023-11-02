2fa2c7549a381637abb5d80aec7e6a0d6fdeb1a0
th3f00 <Akognagster@gmail.com>
2023-11-02 17:22:51 -0400

    commit

--- demo.java
+++ demo.java
@@ -4,7 +4,7 @@ public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello CJ Xiong");
+		System.out.println("hello world! its cj xiong");
 	}
 
 }
